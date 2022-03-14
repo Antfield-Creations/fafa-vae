739a5b89ef68144817054da965572cfe0f84b99b
reinvantveer <reinvantveer@gmail.com>
2022-03-14 11:55:44 +0100

    import additional layers

--- models/train.py
+++ models/train.py
@@ -1,5 +1,9 @@
+from keras.optimizer_v2.adam import Adam
+
+from models.decoder import get_decoder
 from models.encoder import get_encoder
 from models.loaders import Config
+from models.vae import VAE
 
 
 def train(config: Config) -> None:
@@ -13,3 +17,9 @@ def train(config: Config) -> None:
 
     encoder = get_encoder(config)
     encoder.summary()
+
+    decoder = get_decoder(config)
+    decoder.summary()
+
+    vae = VAE(encoder, decoder)
+    vae.compile(optimizer=Adam())
