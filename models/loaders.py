0913c3c9c2711b0d380ed9db1617f6059f58f1fb
reinvantveer <reinvantveer@gmail.com>
2022-03-14 10:31:41 +0100

    replace config type with simple dict

--- models/loaders.py
+++ models/loaders.py
@@ -1,9 +1,7 @@
-from typing import Dict, Union
-
 import yaml
 
-# Type alias for max three-level nested dict
-Config = Dict[str, Dict[str, Union[str, Dict[str, Union[str, int]]]]]
+# Type alias for config type
+Config = dict
 
 
 def load_config(path: str = 'config.yaml') -> Config:
