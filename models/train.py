c0aa89dec9e94542b2ea9d6b91c79020778da43d
reinvantveer <reinvantveer@gmail.com>
2022-03-14 09:48:54 +0100

    bootstrap training package

---
+++ models/train.py
@@ -0,0 +1,13 @@
+from models.loaders import Config
+
+
+def train(config: Config) -> None:
+    """
+    Trains the VAE model on the images
+
+    :param config: a Config object from the load_config function
+
+    :return: None
+    """
+    pass
+
