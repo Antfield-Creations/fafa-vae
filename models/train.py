1a7d080ad67f25f218b99a3ea2d17aef52e7cecb
reinvantveer <rein@vantveer.me>
2022-03-14 20:33:02 +0100

    use image metadata in dataframe to load images as batches
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- models/train.py
+++ models/train.py
@@ -2,7 +2,7 @@ from keras.optimizer_v2.adam import Adam
 
 from models.decoder import get_decoder
 from models.encoder import get_encoder
-from models.loaders import Config
+from models.loaders import Config, FAFADataGenerator, image_metadata
 from models.vae import VAE
 
 
@@ -23,3 +23,9 @@ def train(config: Config) -> None:
 
     vae = VAE(encoder, decoder)
     vae.compile(optimizer=Adam())
+
+    fafa_loader = FAFADataGenerator()
+    metadataframe = image_metadata(config)
+
+    for epoch in range(config['models']['vae']['epochs']):
+        vae.fit(fafa_loader.flow_from_dataframe(dataframe=metadataframe))
