266573b8cbf148eadd9bea9151cfe552f4695c20
reinvantveer <reinvantveer@gmail.com>
2022-03-14 10:07:47 +0100

    allow max three level config dict

--- models/loaders.py
+++ models/loaders.py
@@ -2,8 +2,8 @@ from typing import Dict, Union
 
 import yaml
 
-
-Config = Dict[str, Union[str, int]]
+# Type alias for max three-level nested dict
+Config = Dict[str, Dict[str, Union[str, Dict[str, Union[str, int]]]]]
 
 
 def load_config(path: str = 'config.yaml') -> Config:
