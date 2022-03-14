4f3ac753184eb625e79f0ee0a6052afd66975341
reinvantveer <reinvantveer@gmail.com>
2022-03-14 09:47:48 +0100

    add yaml loader for config DSL

---
+++ models/loaders.py
@@ -0,0 +1,13 @@
+from typing import Dict, Union
+
+import yaml
+
+
+Config = Dict[str, Union[str, int]]
+
+
+def load_config(path='config.yaml') -> Config:
+    with open(path) as f:
+        config = yaml.safe_load(f)
+
+    return config
