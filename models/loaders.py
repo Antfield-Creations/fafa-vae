9b02962339c7fbd48ad5b551c4f318ad3ab698c8
reinvantveer <reinvantveer@gmail.com>
2022-03-15 11:21:07 +0100

    load pandas dataframe from image metadata

--- models/loaders.py
+++ models/loaders.py
@@ -1,3 +1,8 @@
+import json
+import os.path
+from os.path import isfile
+
+import pandas
 import yaml
 
 from keras_preprocessing.image import ImageDataGenerator
@@ -26,5 +31,34 @@ class FAFADataGenerator(ImageDataGenerator):
         )
 
 
-def image_metadata(config: Config) -> DataFrame:
-    pass
+def load_metadata(img_folder: str) -> DataFrame:
+    metadata_path = os.path.join(img_folder, 'metadata.json')
+
+    if not isfile(metadata_path):
+        export_metadata(img_folder)
+
+    df = pandas.read_json(metadata_path)
+    return df
+
+
+def export_metadata(img_folder: str) -> None:
+    metadata = []
+    file_filter = '.jpg'
+    export_path = os.path.join(img_folder, 'metadata.json')
+
+    for root, dirs, files in os.walk(img_folder):
+        relative_path = os.path.relpath(root, img_folder)
+
+        for file in files:
+            if file_filter not in file:
+                continue
+
+            file_base = os.path.splitext(file)[0]
+            tags = file_base.split('-')
+            metadata.append({
+                'filename': os.path.join(relative_path, file),
+                'tags': tags
+            })
+
+    with open(export_path, 'wt') as f:
+        f.write(json.dumps(metadata, indent=2))
---
+++ tests/test_metadata.py
@@ -0,0 +1,38 @@
+import json
+import os.path
+import shutil
+import unittest
+from os.path import isfile
+from tempfile import TemporaryDirectory
+
+from pandas import DataFrame
+
+from models.loaders import export_metadata, load_metadata
+
+
+class MetadataTestCase(unittest.TestCase):
+    def test_metadata_exporter(self):
+        with TemporaryDirectory() as tempdir:
+            shutil.copytree('tests/data', tempdir + '/data')
+            export_metadata(tempdir)
+
+            with self.subTest('It writes the sample files to a metadata json file'):
+                expected_output_path = os.path.join(tempdir, 'metadata.json')
+                self.assertTrue(isfile(expected_output_path))
+
+            with self.subTest('It holds the metadata for the sample files'):
+                with open(expected_output_path, 'rt') as f:
+                    metadata = json.loads(f.read())
+
+                self.assertEqual(len(metadata), 2)
+                self.assertIn('filename', metadata[0].keys())
+                self.assertIn('tags', metadata[0].keys())
+
+    def test_metadata_loader(self):
+        with TemporaryDirectory() as tempdir:
+            shutil.copytree('tests/data', tempdir + '/data')
+
+            with self.subTest('It loads the metadata from a fresh folder without metadata.json'):
+                df = load_metadata(tempdir)
+                self.assertEqual(type(df), DataFrame)
+                self.assertEqual(len(df), 2)
