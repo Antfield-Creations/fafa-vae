500680fb6a660fa765ce4ca4b4942e5708e4dae8
reinvantveer <reinvantveer@gmail.com>
2022-03-15 12:14:08 +0100

    drop epoch loop in favor of passed `epochs` parameter in fit() call

--- models/train.py
+++ models/train.py
@@ -28,11 +28,10 @@ def train(config: Config) -> None:
     img_folder = config['images']['folder']
     metadataframe = load_metadata(img_folder)
 
-    for epoch in range(config['models']['vae']['epochs']):
-        data_generator = fafa_loader.flow_from_dataframe(
-            dataframe=metadataframe,
-            class_mode=None,
-            directory=img_folder,
-            target_size=(config['images']['width'], config['images']['height'])
-        )
-        vae.fit(data_generator)
+    data_generator = fafa_loader.flow_from_dataframe(
+        dataframe=metadataframe,
+        class_mode=None,
+        directory=img_folder,
+        target_size=(config['images']['width'], config['images']['height'])
+    )
+    vae.fit(data_generator, epochs=config['models']['vae']['epochs'])
