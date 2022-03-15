b9397a37e81394fbd7192e23780a474ab365f6a1
reinvantveer <reinvantveer@gmail.com>
2022-03-15 11:28:10 +0100

    refactor: simplify test by using blank test images

--- tests/test_vae_model.py
+++ tests/test_vae_model.py
@@ -1,24 +1,21 @@
 import os.path
+import shutil
 import unittest
 from os import listdir
 from tempfile import TemporaryDirectory
 
-from tqdm import tqdm
-
 from models.loaders import load_config
 from models.train import train
-from scraper.scraper import harvest_set
 
 
 class VAEModelTestCase(unittest.TestCase):
     def test_training(self) -> None:
         with TemporaryDirectory() as tempdir:
+            shutil.copytree('tests/data', tempdir + '/img')
+
             config = load_config()
             config['images']['folder'] = os.path.join(tempdir, 'img')
             config['checkpoints']['folder'] = os.path.join(tempdir, 'checkpoints')
-
-            pbar = tqdm()
-            harvest_set(pbar, 5159, str(config['images']['folder']))
 
             with self.subTest('It generates a checkpoint for a single epoch'):
                 train(config)
