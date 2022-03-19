1631691facfb65bf394ce884b98ee079c7b9b58e
reinvantveer <rein@vantveer.me>
2022-03-19 12:11:08 +0100

    add indexing as option to main
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 This is the 'entrypoint' so to say for running or creating ML models.
 You will need to run the scraper script first if you want to use the FAFA images.
 """
-
+import logging
 from argparse import ArgumentParser
 
-from models.loaders import load_config
+from models.loaders import load_config, load_metadata, export_metadata
 from models.train import train
 from scraper.scraper import scrape
 
 if __name__ == '__main__':
-    known_modes = ['scrape', 'train', 'encode', 'decode']
+    known_modes = ['scrape', 'index', 'train', 'encode', 'decode']
 
     argparser = ArgumentParser(description='Training script for FAFA variational autoencoder')
     argparser.add_argument('-c', '--config', help='Path to the config.yaml file', default='config.yaml')
@@ -21,6 +21,14 @@ if __name__ == '__main__':
 
     if args.mode == 'scrape':
         scrape(config)
+    elif args.mode == 'index':
+        export_metadata(img_folder=config['images']['folder'])
+        metadata = load_metadata(
+            img_folder=config['images']['folder'],
+            exclude_tags=config['images']['filter']['exclude'],
+            include_tags=config['images']['filter']['include'],
+        )
+        logging.info(f'{len(metadata)} images to train on')
     elif args.mode == 'train':
         train(config)
     else:
