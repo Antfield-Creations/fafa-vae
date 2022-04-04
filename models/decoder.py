a474c69d20b7575f3d80970e30250ada64e92c2d
reinvantveer <rein@vantveer.me>
2022-04-04 15:12:40 +0200

    try without any activation
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- models/decoder.py
+++ models/decoder.py
@@ -44,7 +44,7 @@ def get_decoder(config: Config) -> keras.Model:
     decoder_outputs = keras.layers.Conv2DTranspose(
         filters=img_channels,
         kernel_size=3,
-        activation="tanh",
+        # activation="tanh",
         strides=1,
         padding="same",
     )(decoder_layers)
