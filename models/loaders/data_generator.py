3f2e8c6ecb1e575622e4e5ce639b1c43e30f81c1
reinvantveer <reinvantveer@gmail.com>
2022-04-06 10:44:55 +0200

    implement padded image data batch generator
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- models/loaders/data_generator.py
+++ models/loaders/data_generator.py
@@ -1,7 +1,12 @@
-from typing import Generator
+import os.path
+import random
+from math import floor, ceil
+from typing import Generator, Dict, List
 
 import numpy as np
-from keras_preprocessing.image import ImageDataGenerator
+from PIL.Image import Image
+from numpy import ndarray
+from keras_preprocessing.image import ImageDataGenerator, img_to_array, load_img
 
 from models.loaders.config import Config
 from models.loaders.metadata import load_metadata
@@ -50,13 +55,78 @@ def get_generator(config: Config) -> ImageDataGenerator:
 
 def padding_generator(config: Config) -> Generator:
     """
-    Conventional Keras loaders use some kind of interpolation method
+    Conventional Keras loaders use some kind of interpolation method to rescale images to a target size. This one,
+    however, adds padding instead of stretching the image. In this way, the network does not need to learn stretched
+    representations of the domain, instead it can learn the representations directly.
+
+    Hopefully, this will not only help the network learn, but it also drops the requirement of re-scaling reconstructed
+    images to their original, unstretched size.
 
     :param config: The VAE config
 
-    :return: A generator over batches of image tensors
+    :return: An infinite generator over batches of image tensors
     """
 
+    img_folder = config['images']['folder']
+    img_cfg = config['images']
+    batch_size = config['models']['vae']['batch_size']
+
+    img_metadata = load_metadata(
+        img_folder=img_folder,
+        orientation=config['images']['filter']['orientation'],
+        include_tags=config['images']['filter']['include'],
+        exclude_tags=config['images']['filter']['exclude'],
+    )
+
+    record_indices: List[int] = []
     while True:
-        img_cfg = config['images']
-        yield np.ones((config['models']['vae']['batch_size'], img_cfg['height'], img_cfg['width'], img_cfg['channels']))
+        # Keep adding items to the record indices until we have a large enough list to sample a batch
+        while len(record_indices) < batch_size:
+            record_indices.extend(list(range(len(img_metadata))))
+
+        random.shuffle(record_indices)
+
+        batch_indices = [record_indices.pop() for _ in range(batch_size)]
+        batch_meta = img_metadata.iloc[batch_indices]
+
+        img_data = []
+        for record in batch_meta.itertuples():
+            img = load_img(path=os.path.join(img_folder, record.filename))
+            # img_to_array will result in an ndarray of size (height, width, channels)
+            img_values = pad_image(img, img_cfg)
+
+            img_data.append(img_values)
+
+        batch = np.array(img_data)
+
+        batch = batch / 255
+
+        yield batch
+
+
+def pad_image(img: Image, img_cfg: Dict[str, int]) -> ndarray:
+    """
+    Converts an PIL Image instance to a zero-padded numpy ndarray
+
+    :param img:     The Image instance
+    :param img_cfg: A dictionary containing target 'height' and a 'width' keys, with each an integer value
+
+    :return:
+    """
+    img_values = img_to_array(img)
+
+    height_padding = img_cfg['height'] - img.height
+    pad_top = floor(height_padding / 2)
+    pad_bottom = ceil(height_padding / 2)
+    width_padding = img_cfg['width'] - img.width
+    pad_left = floor(width_padding / 2)
+    pad_right = ceil(width_padding / 2)
+
+    img_values = np.pad(
+        array=img_values,
+        pad_width=((pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
+        mode='constant',
+        constant_values=(0, 0)
+    )
+
+    return img_values
