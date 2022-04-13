3399e3b159aa2bd1f4486acf5a0d3ee6867a625c
reinvantveer <reinvantveer@gmail.com>
2022-04-13 12:51:31 +0200

    simplify encoder and decoder models to fit vector-quantized vae
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- models/decoder.py
+++ models/decoder.py
@@ -1,6 +1,6 @@
-import numpy as np
 from tensorflow import keras
 
+from models.encoder import get_encoder
 from models.loaders.config import Config
 
 
@@ -13,39 +13,32 @@ def get_decoder(config: Config) -> keras.Model:
     :return: A Keras model instance as the decoder
     """
 
-    latent_size = config['models']['vae']['latent_size']
+    latent_inputs = keras.Input(shape=get_encoder(config).output.shape[1:])
 
-    latent_inputs = keras.Input(shape=(latent_size,))
-
-    convs = config['models']['vae']['conv2d']
-    img_width = config['images']['width']
-    img_height = config['images']['height']
+    convs = config['models']['vqvae']['conv2d']
+    # The conv2d layers in reverse:
+    convs = list(reversed(convs))
     img_channels = config['images']['channels']
 
-    reductions = np.product([conv['strides'] for conv in convs])
-    width_over_deconv = int(img_width / reductions)
-    height_over_deconv = int(img_height / reductions)
+    decoder_layers = keras.layers.Conv2DTranspose(
+        filters=convs[0]['filters'],
+        kernel_size=convs[0]['kernel_size'],
+        strides=convs[0]['strides'],
+        padding="same",
+    )(latent_inputs)
 
-    decoder_layers = keras.layers.Dense(
-        width_over_deconv * height_over_deconv * convs[-1]['filters'], activation="relu")(latent_inputs)
-    decoder_layers = keras.layers.Reshape(
-        (width_over_deconv, height_over_deconv, convs[-1]['filters']))(decoder_layers)
-
-    # The conv2d layers in reverse:
-    for conv in reversed(convs):
+    for conv in convs[1:]:
         decoder_layers = keras.layers.Conv2DTranspose(
             filters=conv['filters'],
             kernel_size=conv['kernel_size'],
             strides=conv['strides'],
             padding="same",
-            kernel_initializer='random_normal',
         )(decoder_layers)
         decoder_layers = keras.layers.LeakyReLU()(decoder_layers)
 
     decoder_outputs = keras.layers.Conv2DTranspose(
         filters=img_channels,
         kernel_size=3,
-        activation="sigmoid",
         strides=1,
         padding="same",
     )(decoder_layers)
--- models/encoder.py
+++ models/encoder.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 
 from models.loaders.config import Config
-from models.sampling import Sampling
 
 
 def get_encoder(config: Config) -> keras.Model:
@@ -18,30 +17,26 @@ def get_encoder(config: Config) -> keras.Model:
     height = config['images']['height']
     channels = config['images']['channels']
 
-    convs = config['models']['vae']['conv2d']
-    dense = config['models']['vae']['dense']
+    convs = config['models']['vqvae']['conv2d']
 
     inputs = keras.Input(shape=(width, height, channels))
-    # No-op: extract out encoder layers so we can loop over them
-    encoder_layers = keras.layers.Lambda(lambda x: x)(inputs)
 
-    for conv in convs:
+    # Instantiate layers from first convs specification
+    encoder_layers = keras.layers.Conv2D(
+        filters=convs[0]['filters'],
+        kernel_size=convs[0]['kernel_size'],
+        strides=convs[0]['strides'],
+        padding="same",
+    )(inputs)
+
+    for conv in convs[1:]:
         encoder_layers = keras.layers.Conv2D(
             filters=conv['filters'],
             kernel_size=conv['kernel_size'],
             strides=conv['strides'],
             padding="same",
-            kernel_initializer='random_normal',
         )(encoder_layers)
         encoder_layers = keras.layers.LeakyReLU()(encoder_layers)
 
-    encoder_layers = keras.layers.Flatten()(encoder_layers)
-    encoder_layers = keras.layers.Dense(dense['size'], activation="relu")(encoder_layers)
-
-    latent_size = config['models']['vae']['latent_size']
-    z_mean = keras.layers.Dense(latent_size, name="z_mean")(encoder_layers)
-    z_log_var = keras.layers.Dense(latent_size, name="z_log_var")(encoder_layers)
-    z = Sampling()((z_mean, z_log_var))
-
-    encoder = tf.keras.Model(inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder = tf.keras.Model(inputs, encoder_layers, name="encoder")
     return encoder
