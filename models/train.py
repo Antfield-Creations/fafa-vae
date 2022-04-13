e238b25896d9d6206bac827fe28625ff2d2a29b8
reinvantveer <reinvantveer@gmail.com>
2022-04-13 12:50:51 +0200

    use vector-quantized vae implementation
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- models/train.py
+++ models/train.py
@@ -4,6 +4,7 @@ import time
 from logging import getLogger
 from typing import Optional
 
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras.callbacks import History
 
@@ -12,7 +13,7 @@ from models.encoder import get_encoder
 from models.loaders.callbacks import CustomImageSamplerCallback, CustomModelCheckpointSaver, tensorboard_callback
 from models.loaders.config import Config
 from models.loaders.data_generator import padding_generator
-from models.vae import VAE
+from models.vqvae import VQVAETrainer
 
 logger = getLogger('Train')
 
@@ -30,7 +31,7 @@ def train(config: Config, run_id: str = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')) -> 
     encoder = get_encoder(config)
     # Compile the encoder separately to get rid of "uncompiled metrics" warnings
     # See also: https://stackoverflow.com/questions/67970389
-    optimizer = keras.optimizers.Adam(learning_rate=config['models']['vae']['learning_rate'])
+    optimizer = keras.optimizers.Adam(learning_rate=config['models']['vqvae']['learning_rate'])
     encoder.compile(optimizer=optimizer)
     encoder.summary()
 
@@ -38,12 +39,19 @@ def train(config: Config, run_id: str = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')) -> 
     decoder.compile(optimizer=optimizer)
     decoder.summary()
 
-    vae = VAE(encoder, decoder)
-    vae.compile(optimizer=optimizer)
+    data_generator = padding_generator(config)
+
+    variance_sample = []
+    while len(variance_sample) < config['models']['vqvae']['data_generator']['fit_samples']:
+        variance_sample.extend(next(data_generator))
+
+    config['models']['vqvae']['train_variance'] = np.var(variance_sample)
+    vqvae_trainer = VQVAETrainer(config)
+    vqvae_trainer.compile(optimizer=optimizer)
 
     # Checkpoints, sample reconstructions and metric artifact folders
-    artifact_folder = os.path.join(config['models']['vae']['artifacts']['folder'], run_id)
-    logs_folder = config['models']['vae']['artifacts']['logs']['folder'].format(run_id=run_id)
+    artifact_folder = os.path.join(config['models']['vqvae']['artifacts']['folder'], run_id)
+    logs_folder = config['models']['vqvae']['artifacts']['logs']['folder'].format(run_id=run_id)
     models_folder = os.path.join(artifact_folder, 'models')
 
     # Copy model modules to artifacts for archiving
@@ -51,15 +59,14 @@ def train(config: Config, run_id: str = time.strftime('%Y-%m-%d_%Hh%Mm%Ss')) -> 
     shutil.copytree(models_src, models_folder)
     shutil.copy(os.path.join(models_src, '..', 'config.yaml'), os.path.join(models_folder, 'config.yaml'))
 
-    epochs = config['models']['vae']['epochs']
-    steps = config['models']['vae']['batches_per_epoch']
+    epochs = config['models']['vqvae']['epochs']
+    steps = config['models']['vqvae']['batches_per_epoch']
 
-    data_generator = padding_generator(config)
     tensorboard_cb = tensorboard_callback(artifacts_folder=logs_folder)
     image_sampler = CustomImageSamplerCallback(config, run_id)
     checkpoint_saver = CustomModelCheckpointSaver(config, run_id)
 
-    history = vae.fit(
+    history = vqvae_trainer.fit(
         data_generator,
         verbose=1,
         epochs=epochs,
