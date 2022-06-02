71ff3564d048640a93c936b7d3dacb15b9a373a6
reinvantveer <reinvantveer@gmail.com>
2022-06-02 15:38:30 +0200

    lower num_workers, read setting into fit method instead of data generator
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- models/loaders/data_generator.py
+++ models/loaders/data_generator.py
@@ -45,7 +45,6 @@ class PaddingGenerator(Sequence):
         self.img_folder = config['data']['images']['folder']
         self.img_cfg = config['data']['images']
         self.batch_size = config['models']['vqvae']['batch_size']
-        self.num_processes = config['models']['vqvae']['data_generator']['num_workers']
 
         self.img_metadata = load_metadata(
             img_folder=self.img_folder,
