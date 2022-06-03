9a38f17a3f3b42b977810cbf4871d37f8e8a966a
reinvantveer <reinvantveer@gmail.com>
2022-06-03 09:56:16 +0200

    move indexing details into loaders, use dict mapping to map mode as string to function callable with a Config argument
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- main.py
+++ main.py
@@ -2,45 +2,37 @@
 This is the 'entrypoint' so to say for running or creating ML models.
 You will need to run the scraper script first if you want to use the FAFA images.
 """
-import logging
 from argparse import ArgumentParser
+from typing import Dict, Callable
 
+from models import train_vqvae, train_pixelcnn
 from models.loaders.config import load_config, Config
 from models.loaders.data_provision import provision
-from models.loaders.metadata import export_metadata, load_metadata
-from models.train import train
+from models.loaders.metadata import index
 from scraper.scraper import scrape
+
+MODES: Dict[str, Callable[[Config], None]] = {
+    'scrape': scrape,
+    'provision': provision,
+    'index': index,
+    'train-vqvae': train_vqvae.train,
+    'train-pixelcnn': train_pixelcnn.train
+}
 
 
 def main(config: Config, mode: str) -> int:
-    if mode == 'scrape':
-        scrape(config)
-    elif mode == 'provision':
-        provision(config)
-    elif mode == 'index':
-        export_metadata(img_folder=config['data']['images']['folder'])
-        metadata = load_metadata(
-            img_folder=config['data']['images']['folder'],
-            exclude_tags=config['data']['images']['filter']['exclude'],
-            include_tags=config['data']['images']['filter']['include'],
-        )
-        logging.info(f'{len(metadata)} images to train on')
-    elif mode == 'train':
-        train(config)
-
+    MODES[mode](config)
     print("Done!")
     return 0
 
 
 if __name__ == '__main__':
-    known_modes = ['scrape', 'provision', 'index', 'train', 'encode', 'decode']
-
     argparser = ArgumentParser(description='Training script for FAFA variational autoencoder')
     argparser.add_argument('-c', '--config', help='Path to the config.yaml file', default='config.yaml')
-    argparser.add_argument('-m', '--mode', help=f'Mode, one of "{known_modes}"', required=True)
+    argparser.add_argument('-m', '--mode', help=f'Mode, one of "{MODES}"', required=True)
     args = argparser.parse_args()
 
-    if args.mode not in known_modes:
+    if args.mode not in MODES.keys():
         raise NotImplementedError("This mode isn't implemented (yet)")
 
     config = load_config(args.config)
