76077c1ca9c8dc48a6aabec9cf319ea69092ea0c
reinvantveer <reinvantveer@gmail.com>
2022-06-13 11:00:42 +0200

    add fresh model test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- tests/test_vq_vae.py
+++ tests/test_vq_vae.py
@@ -7,6 +7,12 @@ from models.vq_vae import get_vq_vae
 
 
 class VQVAETestCase(unittest.TestCase):
+    def test_fresh_model(self) -> None:
+        config = load_config()
+        config['models']['vq_vae']['artifacts']['resume_model'] = None
+        vq_vae = get_vq_vae(config)
+        self.assertIsInstance(vq_vae, keras.Model)
+
     def test_model_resume_loading(self) -> None:
         config = load_config()
         config['models']['vq_vae']['artifacts']['resume_model'] = 'gs://antfield/test/artifacts/checkpoints/vq_vae/'
