5ba5fd2928ae091b4b55c889dcc7b006e5fa66f6
reinvantveer <reinvantveer@gmail.com>
2022-06-21 14:40:59 +0200

    always use 'Nextcloud' folder for credentials
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,1 +1,1 @@
-GOOGLE_APPLICATION_CREDENTIALS=~/antfield-nextcloud/Documents/fafa-vae-0102b4da1611.json tensorboard --logdir=gs://antfield/FAFA/artifacts
+GOOGLE_APPLICATION_CREDENTIALS=~/Nextcloud/Documents/fafa-vae-0102b4da1611.json tensorboard --logdir=gs://antfield/FAFA/artifacts
