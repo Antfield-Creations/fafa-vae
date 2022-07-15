39ad9d9f1393250163541717f755fb5aac9e532a
reinvantveer <reinvantveer@gmail.com>
2022-07-15 16:19:48 +0200

    test pixelcnn mask generation
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

---
+++ tests/test_pixelcnn.py
@@ -0,0 +1,22 @@
+import unittest
+
+import numpy as np
+
+from models.pixelcnn import PixelConvLayer
+
+
+class PixelCNNTestCase(unittest.TestCase):
+    def test_mask_type_A(self) -> None:
+        kernel_shape = (5, 5, 3)
+        mask = PixelConvLayer.generate_mask(kernel_shape, "A")
+
+        with self.subTest(f'From a {kernel_shape=}, all the row values up to the middle row are ones'):
+            self.assertListEqual(mask.numpy()[:2, ...].tolist(), np.ones(shape=[2, 5, 3]).tolist())  # type: ignore
+
+        with self.subTest(f'From a {kernel_shape=}, all the row values from the middle row + 1 onwards are zeros'):
+            self.assertListEqual(mask.numpy()[3:, ...].tolist(), np.zeros(shape=[2, 5, 3]).tolist())  # type: ignore
+
+        with self.subTest(f'From a {kernel_shape=}, all the middle row values up to the center are ones'):
+            self.assertListEqual(mask.numpy()[2, :2, ...].tolist(), np.ones(shape=[2, 3]).tolist())  # type: ignore
+        with self.subTest(f'From a {kernel_shape=}, all the middle row values up from the center onwards are zeros'):
+            self.assertListEqual(mask.numpy()[2, 2:, ...].tolist(), np.zeros(shape=[3, 3]).tolist())  # type: ignore
